5a5b52c2da332800f206e46841187a2cc41d16d6
adrimartin19 <adry_inter@hotmail.com>
2018-10-21 16:59:08 +0200

    Add files via upload

---
+++ GugelCar1.java
@@ -0,0 +1,11 @@
+//package gugelcar1;
+
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.AgentsConnection;
+
+public class GugelCar1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Radar.java
@@ -0,0 +1,35 @@
+//package gugelcar1;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.SingleAgent;
+
+public class Radar extends SingleAgent {
+   /* 
+    // Estado actual del agente
+    private int status;
+    // Mensajes
+    private ACLMessage inbox, outbox;
+    // Control de la vida del agente
+    private boolean exit;
+    */
+    public Radar(AgentID aid) throws Exception  {
+        super(aid);
+    }
+    
+    @Override
+    public void init()  {
+             
+    }
+    
+    @Override
+    public void execute()  {
+       
+        
+    }
+    
+    @Override
+    public void finalize()  {     
+        super.finalize();
+    }
+}
