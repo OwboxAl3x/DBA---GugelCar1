7f7cfc13d532f4e58793726c758d9ba9cb1fb962
adrimartin19 <adry_inter@hotmail.com>
2018-10-21 17:06:52 +0200

    Esqueleto
    Version Corregida con las clases correctas

---
+++ Perceptor.java
@@ -0,0 +1,36 @@
+//package gugelcar1;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.SingleAgent;
+
+public class Perceptor extends SingleAgent {
+   /* 
+    // Estado actual del agente
+    private int status;
+    // Mensajes
+    private ACLMessage inbox, outbox;
+    // Control de la vida del agente
+    private boolean exit;
+    */
+    public Perceptor
+   (AgentID aid) throws Exception  {
+        super(aid);
+    }
+    
+    @Override
+    public void init()  {
+             
+    }
+    
+    @Override
+    public void execute()  {
+       
+        
+    }
+    
+    @Override
+    public void finalize()  {     
+        super.finalize();
+    }
+}
