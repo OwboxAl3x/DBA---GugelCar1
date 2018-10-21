75baa19a59a57d2ac0030c4e49bd51872d757ec3
adrimartin19 <adry_inter@hotmail.com>
2018-10-21 18:21:19 +0200

    Add files via upload

---
+++ src/guglecar/Coche.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guglecar;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.SingleAgent;
+/**
+ *
+ * @author adri
+ */
+public class Coche extends SingleAgent {
+    /* 
+    // Estado actual del agente
+    private int status;
+    // Mensajes
+    private ACLMessage inbox, outbox;
+    // Control de la vida del agente
+    private boolean exit;
+    */
+    public Coche(AgentID aid) throws Exception  {
+        super(aid);
+    }
+    
+    @Override
+    public void init()  {
+             
+    }
+    
+    @Override
+    public void execute()  {
+       
+        
+    }
+    
+    @Override
+    public void finalize()  {     
+        super.finalize();
+    }
+}
